25572e47fda30786ebef49fe4b873a4e38cb0e62
767829413@qq.com <fangyuanjiushiwo@163.com>
2018-01-21 20:55:44 +0800

    学习XML与PHP的相关操作

---
+++ php_code/test/function.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FY-MrFang
+ * Date: 2018/1/20
+ * Time: 13:02
+ */
+
+    //创建一个curl资源
+    $ch = curl_init();
+    //设置URL与相应选项
+    $url = "http://php.net/manual/en/langref.php";
+    $page = curl_setopt($ch,,$url);
+
+
+
+
+
+
+
+?>
+
+
+
---
+++ php_code/test/mysqli.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FY-MrFang
+ * Date: 2018/1/20
+ * Time: 15:54
+ */
+    echo "111";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
